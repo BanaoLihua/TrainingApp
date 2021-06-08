ff388552ffc5bba88ffdc29d41955b775784a388
banao <tatitachi870@gmail.com>
2021-06-08 18:11:23 +0900

    タブバーの実装

--- App.js
+++ App.js
@@ -1,52 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-
 import Storage from 'react-native-storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { calendarScreen } from './src/calendarScreen';
+import { addNewScreen } from './src/addNewScreen';
 
 export default function App() {
 
-  const storage = new Storage({
-    storageBackend: AsyncStorage,
-  })
-
-  const testSave = () => {
-    storage.save({
-      key: 'test',
-      data: {
-        text: 'banao'
-      }
-    })
-  }
-
-  const testLoad = () => {
-    storage.load({
-      key: 'test',
-    })
-    .then(ret => {
-      console.log(ret.text);
-    })
-    .catch(err => {
-      console.warn(err.message)
-    })
-  }
+  const Tab = createBottomTabNavigator();
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button title="save" onPress={testSave} />
-      <Button title="load" onPress={testLoad} />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="calendar" component={calendarScreen} />
+        <Tab.Screen name="addNew" component={addNewScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/addNewScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export const addNewScreen = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>addNew</Text>
+        </View>
+    );
+}
