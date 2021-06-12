cd224361d9ac7862f3bee299b10a9890b0058bae
banao <tatitachi870@gmail.com>
2021-06-12 22:21:06 +0900

    新規作成画面で色選択を追加（静的）

--- src/addNewScreen.js
+++ src/addNewScreen.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 //import { saveData } from './store';
 import { useNavigation } from '@react-navigation/native';
-import { Button, TextInput } from 'react-native-paper';
+//import { Button, TextInput } from 'react-native-paper';
 import Storage from 'react-native-storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Header, Input, Text, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export const addNewScreen = () => {
 
@@ -13,6 +16,8 @@ export const addNewScreen = () => {
     const [oldData, setOldData] = useState({
         items: {}
     });
+
+    const [part, setPart] = useState('');
 
     const loadData = () => {
         storage.load({key: 'item'})
@@ -35,6 +40,7 @@ export const addNewScreen = () => {
 
     // 保存処理
     const onPressSave = () => {
+        console.log(part);
         const keyName = getNowYMD();
         oldData.items[keyName] = [{name: text}];
         storage.save({
@@ -52,13 +58,36 @@ export const addNewScreen = () => {
 
     const navigation = useNavigation();
     return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-            <TextInput value={text}
-                       mode='outlined'
-                       onChangeText={text => setText(text)}
-                       style={{ margin: 30 }} />
-            <Button mode="contained"
-                    color="dodgerblue"
+        <View style={{ flex: 1 }}>
+            <Header 
+                centerComponent={{ text: '今日の成果', style: { color: '#fff', fontSize: 20 } }}
+                rightComponent={{ icon: 'home', color: '#fff' }}
+            />
+            <Input value={text}
+                   label="Title"
+                   onChangeText={text => setText(text)}
+            />
+            <Input
+                value="50"
+                label="Weight"
+                keyboardType="numeric"
+            />
+            <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 10}}>
+                
+                <Icon name="circle" size={30} color="tomato" onPress={() => setPart('shoulder')} />
+                <Text h3>肩</Text>
+                <Icon name="circle" size={30} color="orange" onPress={() => setPart('arm')} style={{marginLeft: 15}} />
+                <Text h3>腕</Text>
+                <Icon name="circle" size={30} color="lightgreen" onPress={() => setPart('chest')} style={{marginLeft: 15}} />
+                <Text h3>胸</Text>
+                <Icon name="circle" size={30} color="lightblue" onPress={() => setPart('stomach')} style={{marginLeft: 15}} />
+                <Text h3>腹</Text>
+                <Icon name="circle" size={30} color="plum" onPress={() => setPart('back')} style={{marginLeft: 15}} />
+                <Text h3>背</Text>
+                
+
+            </View>
+            <Button title="保存"
                     style={{ margin: 30 }}
                     onPress={ onPressSave }
                     >保存</Button>
