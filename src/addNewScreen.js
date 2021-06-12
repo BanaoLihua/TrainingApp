7f57e13aa553fbcf8b12bcdc2b3909e7a2c386d8
banao <tatitachi870@gmail.com>
2021-06-13 01:33:05 +0900

    新規作成画面で部位選択を動的に実装

--- src/addNewScreen.js
+++ src/addNewScreen.js
@@ -7,6 +7,7 @@ import Storage from 'react-native-storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Header, Input, Text, Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { FlatList } from 'react-native-gesture-handler';
 
 
 export const addNewScreen = () => {
@@ -17,7 +18,23 @@ export const addNewScreen = () => {
         items: {}
     });
 
+    // 部位選択
     const [part, setPart] = useState('');
+
+    const [mode, setMode] = useState('circle');
+
+    const [selectedItems, setSelectedItems] = useState({ shoulder: false, arm: false, chest: false, stomach: false, back: false });
+
+    const [isSelectedShoulder, setIsSelectedShoulder] = useState('circle');
+
+    const [isSelectedArm, setIsSelectedArm] = useState('circle');
+
+    const [isSelectedChest, setIsSelectedChest] = useState('circle');
+
+    const [isSelectedStomach, setIsSelectedStomach] = useState('circle');
+
+    const [isSelectedBack, setIsSelectedBack] = useState('circle');
+    //
 
     const loadData = () => {
         storage.load({key: 'item'})
@@ -40,7 +57,6 @@ export const addNewScreen = () => {
 
     // 保存処理
     const onPressSave = () => {
-        console.log(part);
         const keyName = getNowYMD();
         oldData.items[keyName] = [{name: text}];
         storage.save({
@@ -55,6 +71,44 @@ export const addNewScreen = () => {
         });
         return unsubscribe;
     }, [navigation]);
+
+    // 部位選択
+    // Todo: case分の中の三項方程式の正負が逆な件
+    const selectPart = (part) => {
+        switch(part) {
+            case 'shoulder':
+                setSelectedItems({ ...selectedItems, shoulder: !selectedItems.shoulder });
+                //selectedItems.shoulder ? setIsSelectedShoulder('check-circle') : setIsSelectedShoulder('circle');
+                selectedItems.shoulder ? setIsSelectedShoulder('circle') : setIsSelectedShoulder('check-circle');
+                break;
+            case 'arm':
+                setSelectedItems({ ...selectedItems, arm: !selectedItems.arm });
+                //selectedItems.arm ? setIsSelectedArm('check-circle') : setIsSelectedArm('circle');
+                selectedItems.arm ? setIsSelectedArm('circle') : setIsSelectedArm('check-circle');
+                break;
+            case 'chest':
+                setSelectedItems({ ...selectedItems, chest: !selectedItems.chest });
+                //selectedItems.chest ? setIsSelectedChest('check-circle') : setIsSelectedChest('circle');
+                selectedItems.chest ? setIsSelectedChest('circle') : setIsSelectedChest('check-circle');
+                break;
+            case 'stomach':
+                setSelectedItems({ ...selectedItems, stomach: !selectedItems.stomach });
+                //selectedItems.stomach ? setIsSelectedChest('check-circle') : setIsSelectedChest('circle');
+                selectedItems.stomach ? setIsSelectedStomach('circle') : setIsSelectedStomach('check-circle');
+                break;
+            case 'back':
+                setSelectedItems({ ...selectedItems, back: !selectedItems.back });
+                //selectedItems.back ? setIsSelectedChest('check-circle') : setIsSelectedChest('circle');
+                selectedItems.back ? setIsSelectedBack('circle') : setIsSelectedBack('check-circle');
+                break;
+        }
+        
+    }
+
+    const onPressTest = () => {
+        console.log(selectedItems);
+        console.log(isSelectedShoulder)
+    }
 
     const navigation = useNavigation();
     return (
@@ -74,23 +128,28 @@ export const addNewScreen = () => {
             />
             <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 10}}>
                 
-                <Icon name="circle" size={30} color="tomato" onPress={() => setPart('shoulder')} />
+                <Icon name={isSelectedShoulder} size={30} color="tomato" onPress={() => selectPart('shoulder')} />
                 <Text h3>肩</Text>
-                <Icon name="circle" size={30} color="orange" onPress={() => setPart('arm')} style={{marginLeft: 15}} />
+                <Icon name={isSelectedArm} size={30} color="orange" onPress={() => selectPart('arm')} style={{marginLeft: 15}} />
                 <Text h3>腕</Text>
-                <Icon name="circle" size={30} color="lightgreen" onPress={() => setPart('chest')} style={{marginLeft: 15}} />
+                <Icon name={isSelectedChest} size={30} color="lightgreen" onPress={() => selectPart('chest')} style={{marginLeft: 15}} />
                 <Text h3>胸</Text>
-                <Icon name="circle" size={30} color="lightblue" onPress={() => setPart('stomach')} style={{marginLeft: 15}} />
+                <Icon name={isSelectedStomach} size={30} color="lightskyblue" onPress={() => selectPart('stomach')} style={{marginLeft: 15}} />
                 <Text h3>腹</Text>
-                <Icon name="circle" size={30} color="plum" onPress={() => setPart('back')} style={{marginLeft: 15}} />
+                <Icon name={isSelectedBack} size={30} color="plum" onPress={() => selectPart('back')} style={{marginLeft: 15}} />
                 <Text h3>背</Text>
                 
 
             </View>
+            
+            <Button title="テスト"
+                    style={{ margin: 30 }}
+                    onPress={ onPressTest }
+            />
             <Button title="保存"
                     style={{ margin: 30 }}
                     onPress={ onPressSave }
-                    >保存</Button>
+            />
         </View>
     );
 }
