89fd389c2135bae627ee02fe83b0fe7a29d84e3b
banao <tatitachi870@gmail.com>
2021-06-13 18:54:06 +0900

    冗長な定数定義を解消

--- src/addNewScreen.js
+++ src/addNewScreen.js
@@ -7,7 +7,6 @@ import Storage from 'react-native-storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Header, Input, Text, Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { FlatList } from 'react-native-gesture-handler';
 
 
 export const addNewScreen = () => {
@@ -19,21 +18,13 @@ export const addNewScreen = () => {
     });
 
     // 部位選択
-    const [part, setPart] = useState('');
-
-    const [mode, setMode] = useState('circle');
-
-    const [selectedItems, setSelectedItems] = useState({ shoulder: false, arm: false, chest: false, stomach: false, back: false });
-
-    const [isSelectedShoulder, setIsSelectedShoulder] = useState('circle');
-
-    const [isSelectedArm, setIsSelectedArm] = useState('circle');
-
-    const [isSelectedChest, setIsSelectedChest] = useState('circle');
-
-    const [isSelectedStomach, setIsSelectedStomach] = useState('circle');
-
-    const [isSelectedBack, setIsSelectedBack] = useState('circle');
+    const [selectedItems, setSelectedItems] = useState({ 
+        shoulder: [false, 'circle'], 
+        arm: [false, 'circle'], 
+        chest: [false, 'circle'], 
+        stomach: [false, 'circle'], 
+        back: [false, 'circle']
+    });
     //
 
     const loadData = () => {
@@ -77,29 +68,44 @@ export const addNewScreen = () => {
     const selectPart = (part) => {
         switch(part) {
             case 'shoulder':
-                setSelectedItems({ ...selectedItems, shoulder: !selectedItems.shoulder });
-                //selectedItems.shoulder ? setIsSelectedShoulder('check-circle') : setIsSelectedShoulder('circle');
-                selectedItems.shoulder ? setIsSelectedShoulder('circle') : setIsSelectedShoulder('check-circle');
+                setSelectedItems({ 
+                    ...selectedItems, 
+                    shoulder: [!selectedItems.shoulder[0], 
+                                selectedItems.shoulder[0] ? selectedItems.shoulder[1] = 'circle' : selectedItems.shoulder[1] = 'check-circle'
+                              ] 
+                });
                 break;
             case 'arm':
-                setSelectedItems({ ...selectedItems, arm: !selectedItems.arm });
-                //selectedItems.arm ? setIsSelectedArm('check-circle') : setIsSelectedArm('circle');
-                selectedItems.arm ? setIsSelectedArm('circle') : setIsSelectedArm('check-circle');
+                setSelectedItems({ 
+                    ...selectedItems, 
+                    arm: [!selectedItems.arm[0], 
+                                selectedItems.arm[0] ? selectedItems.arm[1] = 'circle' : selectedItems.arm[1] = 'check-circle'
+                         ] 
+                });
                 break;
             case 'chest':
-                setSelectedItems({ ...selectedItems, chest: !selectedItems.chest });
-                //selectedItems.chest ? setIsSelectedChest('check-circle') : setIsSelectedChest('circle');
-                selectedItems.chest ? setIsSelectedChest('circle') : setIsSelectedChest('check-circle');
+                setSelectedItems({ 
+                    ...selectedItems, 
+                    chest: [!selectedItems.chest[0], 
+                                selectedItems.chest[0] ? selectedItems.chest[1] = 'circle' : selectedItems.chest[1] = 'check-circle'
+                           ] 
+                });
                 break;
             case 'stomach':
-                setSelectedItems({ ...selectedItems, stomach: !selectedItems.stomach });
-                //selectedItems.stomach ? setIsSelectedChest('check-circle') : setIsSelectedChest('circle');
-                selectedItems.stomach ? setIsSelectedStomach('circle') : setIsSelectedStomach('check-circle');
+                setSelectedItems({ 
+                    ...selectedItems, 
+                    stomach: [!selectedItems.stomach[0], 
+                                selectedItems.stomach[0] ? selectedItems.stomach[1] = 'circle' : selectedItems.stomach[1] = 'check-circle'
+                             ] 
+                });
                 break;
             case 'back':
-                setSelectedItems({ ...selectedItems, back: !selectedItems.back });
-                //selectedItems.back ? setIsSelectedChest('check-circle') : setIsSelectedChest('circle');
-                selectedItems.back ? setIsSelectedBack('circle') : setIsSelectedBack('check-circle');
+                setSelectedItems({ 
+                    ...selectedItems, 
+                    back: [!selectedItems.back[0], 
+                                selectedItems.back[0] ? selectedItems.back[1] = 'circle' : selectedItems.back[1] = 'check-circle'
+                          ] 
+                });
                 break;
         }
         
@@ -107,7 +113,6 @@ export const addNewScreen = () => {
 
     const onPressTest = () => {
         console.log(selectedItems);
-        console.log(isSelectedShoulder)
     }
 
     const navigation = useNavigation();
@@ -128,15 +133,15 @@ export const addNewScreen = () => {
             />
             <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 10}}>
                 
-                <Icon name={isSelectedShoulder} size={30} color="tomato" onPress={() => selectPart('shoulder')} />
+                <Icon name={selectedItems.shoulder[1]} size={30} color="tomato" onPress={() => selectPart('shoulder')} />
                 <Text h3>肩</Text>
-                <Icon name={isSelectedArm} size={30} color="orange" onPress={() => selectPart('arm')} style={{marginLeft: 15}} />
+                <Icon name={selectedItems.arm[1]} size={30} color="orange" onPress={() => selectPart('arm')} style={{marginLeft: 15}} />
                 <Text h3>腕</Text>
-                <Icon name={isSelectedChest} size={30} color="lightgreen" onPress={() => selectPart('chest')} style={{marginLeft: 15}} />
+                <Icon name={selectedItems.chest[1]} size={30} color="lightgreen" onPress={() => selectPart('chest')} style={{marginLeft: 15}} />
                 <Text h3>胸</Text>
-                <Icon name={isSelectedStomach} size={30} color="lightskyblue" onPress={() => selectPart('stomach')} style={{marginLeft: 15}} />
+                <Icon name={selectedItems.stomach[1]} size={30} color="lightskyblue" onPress={() => selectPart('stomach')} style={{marginLeft: 15}} />
                 <Text h3>腹</Text>
-                <Icon name={isSelectedBack} size={30} color="plum" onPress={() => selectPart('back')} style={{marginLeft: 15}} />
+                <Icon name={selectedItems.back[1]} size={30} color="plum" onPress={() => selectPart('back')} style={{marginLeft: 15}} />
                 <Text h3>背</Text>
                 
 
